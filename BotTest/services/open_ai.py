5c4a1e60dabfeb99098eccf5a92039c69ffb8abe
ftn92001 <ftn92001@gmail.com>
2023-05-28 17:42:36 +0800

    openai add previous content

--- BotTest/services/open_ai.py
+++ BotTest/services/open_ai.py
@@ -1,7 +1,17 @@
 import requests
 from LineBot.settings import OPEN_AI_API_KEY
 
+prev_content = ''
+prev_answer = ''
+
 def call_completions(content):
+    global prev_content
+    global prev_answer
+    messages = [
+        {'role': 'user', 'content': prev_content},
+        {'role': 'user', 'content': prev_answer},
+        {'role': 'user', 'content': content},
+    ]
     response = requests.post(
         'https://api.openai.com/v1/chat/completions',
         headers = {
@@ -10,10 +20,14 @@ def call_completions(content):
         },
         json = {
             'model': 'gpt-3.5-turbo',
-            'messages': [{'role': 'user', 'content': content}],
+            'messages': messages,
             'temperature': 0.6,
             'max_tokens': 3000
         }
     )
+    answer = response.json()['choices'][0]['message']['content']
 
-    return response.json()['choices'][0]['message']['content'].replace('\n',' ')
+    prev_content = content
+    prev_answer = answer
+    print(response.json())
+    return answer.replace('\n',' ')
